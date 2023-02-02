af5d0124b5510bf96d1d40d74df545b84755faa8
D3TONAT0R <gdv@swissonline.ch>
2023-02-02 18:44:37 +0100

    Add decorations to maze

--- Assets/Maze/Scripts/MazeBuilder.cs
+++ Assets/Maze/Scripts/MazeBuilder.cs
@@ -4,163 +4,190 @@ using UnityEngine;
 using MazeGen;
 using D3T;
 
-public class MazeBuilder : MonoBehaviour
+namespace TwoWorlds
 {
-	[System.Serializable]
-	public class MazePiecePrefabs
+	public class MazeBuilder : MonoBehaviour
 	{
-		public GameObject solid;
-		public GameObject _0000;
-		public GameObject _0001;
-		public GameObject _0010;
-		public GameObject _0011;
-		public GameObject _0100;
-		public GameObject _0101;
-		public GameObject _0110;
-		public GameObject _0111;
-		public GameObject _1000;
-		public GameObject _1001;
-		public GameObject _1010;
-		public GameObject _1011;
-		public GameObject _1100;
-		public GameObject _1101;
-		public GameObject _1110;
-		public GameObject _1111;
-
-		public GameObject GetPiece(int i)
+		[System.Serializable]
+		public class MazePiecePrefabs
 		{
-			switch(i)
+			public GameObject solid;
+			public GameObject _0000;
+			public GameObject _0001;
+			public GameObject _0010;
+			public GameObject _0011;
+			public GameObject _0100;
+			public GameObject _0101;
+			public GameObject _0110;
+			public GameObject _0111;
+			public GameObject _1000;
+			public GameObject _1001;
+			public GameObject _1010;
+			public GameObject _1011;
+			public GameObject _1100;
+			public GameObject _1101;
+			public GameObject _1110;
+			public GameObject _1111;
+
+			public GameObject GetPiece(int i)
 			{
-				case 0b0000: return _0000;
-				case 0b0001: return _0001;
-				case 0b0010: return _0010;
-				case 0b0011: return _0011;
-				case 0b0100: return _0100;
-				case 0b0101: return _0101;
-				case 0b0110: return _0110;
-				case 0b0111: return _0111;
-				case 0b1000: return _1000;
-				case 0b1001: return _1001;
-				case 0b1010: return _1010;
-				case 0b1011: return _1011;
-				case 0b1100: return _1100;
-				case 0b1101: return _1101;
-				case 0b1110: return _1110;
-				case 0b1111: return _1111;
-				default: throw new System.IndexOutOfRangeException();
+				switch(i)
+				{
+					case 0b0000: return _0000;
+					case 0b0001: return _0001;
+					case 0b0010: return _0010;
+					case 0b0011: return _0011;
+					case 0b0100: return _0100;
+					case 0b0101: return _0101;
+					case 0b0110: return _0110;
+					case 0b0111: return _0111;
+					case 0b1000: return _1000;
+					case 0b1001: return _1001;
+					case 0b1010: return _1010;
+					case 0b1011: return _1011;
+					case 0b1100: return _1100;
+					case 0b1101: return _1101;
+					case 0b1110: return _1110;
+					case 0b1111: return _1111;
+					default: throw new System.IndexOutOfRangeException();
+				}
 			}
 		}
-	}
 
-	public MazePiecePrefabs mazePiecePrefabs;
-	public bool clear;
-	public bool regenerate;
+		public MazePiecePrefabs mazePiecePrefabs;
+		[Range(0, 0.5f)]
+		public float decorationAmount = 0.1f;
+		public WeightedGameObject[] decorations;
 
-	[Space(20)]
-	[Range(4, 16)]
-	public int width = 8;
-	[Range(4, 16)]
-	public int height = 8;
-	public float mazeScale = 5;
+		public bool autoRegenerate;
 
-	[Space(20)]
-	public bool randomSeed;
-	public int seed = 1234;
+		[Space(20)]
+		[Range(4, 16)]
+		public int width = 8;
+		[Range(4, 16)]
+		public int height = 8;
+		public float mazeScale = 5;
 
-	[Space(20)]
-	[Range(0, 1)]
-	public float complexity = 0.7f;
-	[Range(0, 8)]
-	public int fillIterations = 4;
-	[Range(0, 1)]
-	public float breakup = 0.1f;
-	public bool requireExit;
+		[Space(20)]
+		public bool randomSeed;
+		public int seed = 1234;
 
-	[Space(20)]
-	public bool openStart;
-	public bool openEnd;
+		[Space(20)]
+		[Range(0, 1)]
+		public float complexity = 0.7f;
+		[Range(0, 8)]
+		public int fillIterations = 4;
+		[Range(0, 1)]
+		public float breakup = 0.1f;
+		public bool requireExit;
 
-	void Start()
-	{
-		Generate();
-	}
+		[Space(20)]
+		public bool openStart;
+		public bool openEnd;
 
-	private void OnValidate()
-	{
-		if(regenerate)
+		void Start()
 		{
-			this.InvokeValidation(() => Generate());
-		}
-		if(clear)
-		{
-			this.InvokeValidation(() => Clear());
-		}
-	}
-
-	public void Clear()
-	{
-		clear = false;
-		for(int i = transform.childCount - 1; i >= 0; i--)
-		{
-			DestroyImmediate(transform.GetChild(i).gameObject);
-		}
-	}
-
-	public void Generate()
-	{
-		regenerate = false;
-		Clear();
-		if(randomSeed)
-		{
-			seed = Random.Range(0, 10000);
-		}
-		MazeGenerator gen = new MazeGenerator(width, height)
-		{
-			mazeComplexity = complexity,
-			seed = seed,
-			maxEmptySpaceFillIterations = fillIterations,
-			openStart = openStart,
-			openEnd = openEnd
-		};
-
-		var maze = gen.Generate(requireExit, out _);
-		if(breakup > 0)
-		{
-			gen.Breakup(breakup);
+			Generate();
+			Decorate();
 		}
 
-		if(maze != null)
+		private void OnValidate()
 		{
-			for(int x = 0; x < width; x++)
+			if(autoRegenerate)
 			{
-				for(int z = 0; z < height; z++)
+				this.InvokeValidation(() => Generate());
+			}
+		}
+
+		public void Clear()
+		{
+			for(int i = transform.childCount - 1; i >= 0; i--)
+			{
+				DestroyImmediate(transform.GetChild(i).gameObject);
+			}
+		}
+
+		[ContextMenu("Generate Maze")]
+		public void Generate()
+		{
+			BuildMaze();
+			Decorate();
+		}
+
+		private void BuildMaze()
+		{
+			Clear();
+			if(randomSeed)
+			{
+				seed = Random.Range(0, 10000);
+			}
+			MazeGenerator gen = new MazeGenerator(width, height)
+			{
+				mazeComplexity = complexity,
+				seed = seed,
+				maxEmptySpaceFillIterations = fillIterations,
+				openStart = openStart,
+				openEnd = openEnd
+			};
+
+			var maze = gen.Generate(requireExit, out _);
+			if(breakup > 0)
+			{
+				gen.Breakup(breakup);
+			}
+
+			if(maze != null)
+			{
+				for(int x = 0; x < width; x++)
 				{
-					var piece = maze.GetPieceAt(new MazeVector(x, z));
-					int pieceIndex = 0;
-					GameObject prefab;
-					if(piece != null)
+					for(int z = 0; z < height; z++)
 					{
-						if(piece.north) pieceIndex += 1 << 3;
-						if(piece.east) pieceIndex += 1 << 2;
-						if(piece.south) pieceIndex += 1 << 1;
-						if(piece.west) pieceIndex += 1 << 0;
-						prefab = mazePiecePrefabs.GetPiece(pieceIndex);
+						var piece = maze.GetPieceAt(new MazeVector(x, z));
+						int pieceIndex = 0;
+						GameObject prefab;
+						if(piece != null)
+						{
+							if(piece.north) pieceIndex += 1 << 3;
+							if(piece.east) pieceIndex += 1 << 2;
+							if(piece.south) pieceIndex += 1 << 1;
+							if(piece.west) pieceIndex += 1 << 0;
+							prefab = mazePiecePrefabs.GetPiece(pieceIndex);
+						}
+						else
+						{
+							prefab = mazePiecePrefabs.solid;
+						}
+						if(prefab)
+						{
+							var instance = Instantiate(prefab, transform);
+							instance.transform.localPosition = new Vector3(x * mazeScale, 0, z * mazeScale);
+						}
 					}
-					else
+				}
+			}
+			else
+			{
+				Debug.LogError("Maze gen failed");
+			}
+		}
+
+		private void Decorate()
+		{
+			for(int z = 0; z < height; z++)
+			{
+				for(int x = 0; x < width; x++)
+				{
+					if(RandomUtilities.Probability(decorationAmount))
 					{
-						prefab = mazePiecePrefabs.solid;
-					}
-					if(prefab)
-					{
-						var instance = Instantiate(prefab, transform);
-						instance.transform.localPosition = new Vector3(x * mazeScale, 0, z * mazeScale);
+						var prefab = WeightedGameObject.PickRandomFromArray(decorations);
+						if(prefab)
+						{
+							var instance = Instantiate(prefab, transform);
+							instance.transform.localPosition = new Vector3(x * mazeScale, 0, z * mazeScale);
+						}
 					}
 				}
 			}
 		}
-		else
-		{
-			Debug.LogError("Maze gen failed");
-		}
-	}
+	} 
 }
---
+++ Assets/Scripts/WeightedGameObject.cs
@@ -0,0 +1,24 @@
+﻿using D3T;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TwoWorlds
+{
+	[System.Serializable]
+	public struct WeightedGameObject
+	{
+		public float weight;
+		public GameObject gameObject;
+
+		public static GameObject PickRandomFromArray(WeightedGameObject[] array)
+		{
+			float[] weights = new float[array.Length];
+			for(int i = 0; i < array.Length; i++)
+			{
+				weights[i] = array[i].weight;
+			}
+			return array[RandomUtilities.PickRandomWeighted(weights)].gameObject;
+		}
+	} 
+}
