bae2c6f5b9ba3786085c64c6512164355607838d
D3TONAT0R <gdv@swissonline.ch>
2023-02-04 19:37:17 +0100

    Add proper scene transitions

---
+++ Assets/Scripts/IntroPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TwoWorlds
+{
+	public class IntroPlayer : MonoBehaviour
+	{
+		private void Awake()
+		{
+			if(GameLevelLoader.mazeLevel == 0) GameLevelLoader.mazeLevel = 1;
+			var animator = GetComponent<Animator>();
+			if(GameLevelLoader.mazeLevel == 1)
+			{
+				animator.Play("Intro");
+			}
+			else
+			{
+				animator.Play("Transition");
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Triggers/CheckMazeLevel.cs
@@ -0,0 +1,19 @@
+﻿using D3T;
+using D3T.Triggers.Conditions;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TwoWorlds
+{
+	public class CheckMazeLevel : TriggerCondition
+	{
+		public ComparisonOperator comparison;
+		public int level;
+
+		protected override bool Evaluate()
+		{
+			return comparison.Compare(GameLevelLoader.mazeLevel, level);
+		}
+	}
+}
